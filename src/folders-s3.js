80ba764d35e53e532366573b315c292ab849ec82
anitahitouch <anitahitouch@gmail.com>
2016-01-12 23:45:01 +0530

    Changes related to list upload and download from local s3 server

--- src/folders-s3.js
+++ src/folders-s3.js
@@ -1,14 +1,15 @@
 var uriParse = require('url');
-var AWS = require('aws-sdk');
 var path = require('path');
 var fs = require('fs');
+var FoldersAws = require('folders-aws');
+var AWS = require('aws-sdk');
 
 var FoldersS3 = function(prefix,options){
 	console.log("FoldersS3");
 	options = options || {};
 	options.connectionString = options.connectionString || 'http://localhost:4568/';
-	options.silent = options.silent || true ;
-	options.directory = options.directory || '/tmp/s3rver_test_directory';
+	//options.silent = options.silent || true ;
+	options.directory = options.directory || './dir';
 	this.prefix = prefix;
 	this.client = null;
 	
@@ -43,79 +44,63 @@ FoldersS3.prototype.prepare = function(){
   		s3ForcePathStyle: true,
   		accessKeyId: 'ACCESS_KEY_ID',
   		secretAccessKey: 'SECRET_ACCESS_KEY',
-  		endpoint: new AWS.Endpoint('http://localhost:4568')
+  		endpoint: new AWS.Endpoint('http://localhost:4568'),
+		service         : "S3",
+    	region         : "us-west-2",
+    	bucket          : "bucket1",
 	};
 
-	self.client = new AWS.S3(config);
+	self.client = new FoldersAws('prefix',config);
+	//directly using aws sdk to access methods like createbucket which are not available
+	// in folders.io folders-aws client
+	//self.client2 = new AWS.S3(config);
 };
 	
 FoldersS3.prototype.ls = function(path,cb){
-
-	var result;
+	
 	var self = this;
 	self.prepare();
-    self.client.listObjects({
-        Bucket: 'some bucket',
-        Prefix: 'some prefix'
-    }, function (err, data) {
-
-
-        if (err) {
-            console.log("error occured in folders-aws lsBucket() ", err);
-            return cb(err, null);
-
-        } else {
-            result = data.Contents;
-            return cb(null, result);
-
-        }
-    });
+	self.client.ls(path,cb);
 	
 };
 
 FoldersS3.prototype.cat = function(path,cb){
 	var self = this;
 	self.prepare();
-	var params = {
-        Bucket: 'some bucket',
-        /* required */
-
-        Key: 'some key' /* required */
-    };
-	
-    // FIXME: See if we can get some info on the remote file, esp. length.
-    // headObject / listObjects  works well enough usually.
-
-	var f = self.client.getObject(params);
-			
-    var file = f.createReadStream();
-            
-    cb(null, {
-                stream: file,
-                size: 'some size',
-                name: path.basename(key)
-    });
-
-         // successful response
+	self.client.cat(path,cb);
    
-	
 };
 
 FoldersS3.prototype.write = function(path,data,cb){
 	var self = this;
 	self.prepare();
-	// for now uploading stub data only .path data will be ignored 
-	var params = {
-  		Key: 'Key',
- 		Bucket: 'Bucket',
-  		Body: fs.createReadStream('./tyu.jpg')
-	};
-
-	self.client.upload(params, function uploadCallback (err, data) {
-  		console.log(err, data)
-	});
+	self.client.write(path,data,cb);
 };
 
+FoldersS3.prototype.mkdir = function(path,cb){
+	var self = this;
+	self.prepare();
+	self.client.mkdir(path,cb);
+	
+};
+/*
+// FIXME : bucket creation is not supported in folders-aws so using official aws sdk 
+FoldersS3.prototype.createTestBucket = function(bucket,cb){
+	var self = this;
+	var params = {
+  		Bucket: bucket, /* required 
+  
+	};
+	self.prepare();
+	self.client2.createBucket(params, function(err, data) {
+  		if (err){
+			cb(err, err.stack);
+		}// an error occurred
+  		else  {   cb(null,data) 
+		}// successful response
+	});
+};
+*/
 var parseConnString = function(connectionString){
 	var uri = uriParse.parse(connectionString, true);
 	var conn = {
--- src/test.js
+++ src/test.js
@@ -2,16 +2,36 @@
  * This file contains code for testing aws s3 test server 
  */
 var fs = require('fs')
+var t = require('folders-aws');
 
 var FoldersS3 = require('./folders-s3');
 
 var o = new FoldersS3();
 
-o.write('/path','some',function(err,res){
+// listing a bucket from edded s3 s3rver 
+
+		o.ls('/S3/us-west-2/bucket1/',function(err,res){
+		
+			if (!err)
+				console.log(res);
+		});
 	
-	if(err){
-	
-		console.log(err);
-	}
-	console.log(res);
-});
+
+// downloading a file from embeddeddded s3 s3rver 
+
+		o.cat('/S3/us-west-2/bucket1/',function(err,res){
+		
+			if (!err)
+				console.log(res);
+		});
+
+// uploading a file to embedded s3 s3rver 
+
+		o.write('/S3/us-west-2/bucket1/',require('fs').createReadStream('./tyu.jpg'),function(err,res){
+		
+			if (!err)
+				console.log(res);
+		});
+
+
+
